de7858aea3fed7f9e48daea75ae314691bc35093
sudoentropy <sudoentropy@gmail.com>
2021-01-21 23:15:56 -0500

    insertion sort

--- ins_sort.c
+++ ins_sort.c
@@ -3,9 +3,23 @@
 #include <math.h>
 #include <stdio.h>
 
-void insertionSort(int arr[], int n){
+void insertionSort(int arr[], int n) {
 	
 	int i, key, j;
-	for (i = 1; i < n; i++)
+	for (i = 1; i < n; i++) {
+		key = arr[i];
+		j = i - 1;
 
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+
+}
+
+//appraently C pprograms cant compile without this...lolz
+int main() {
+	return 0;
 }
