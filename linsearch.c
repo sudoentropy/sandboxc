b47e4164aad680830dc875ed0a7d562cd16ad415
sudoentropy <sudoentropy@gmail.com>
2021-01-25 10:48:54 -0500

    learning teh codez

---
+++ linsearch.c
@@ -0,0 +1,25 @@
+// in linear search we are walking through all an array sequentially
+// and testing for a an equality match, maybe a bool
+// data structures needed, int and array
+//pseduocode below:
+
+
+prcedure liearSearch(Array, Key)
+
+	for element in Array;
+		if search element == Key;
+		
+			return element Index;
+
+		else
+			
+			return nil
+
+		end if
+	
+	end for
+
+end procedure
+
+
+
